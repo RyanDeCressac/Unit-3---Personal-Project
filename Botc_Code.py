fef8d99caf1f14f016bd1540e3af6c83fb1c1cff
RyanDeCressac <ryan.decressac@education.nsw.gov.au>
2025-04-10 17:16:40 +1000

    - Created a html webpage to submit data using a css sheet to style it - Fixed an issue where the program will crash while verifying input data

--- Botc_Code.py
+++ Botc_Code.py
@@ -1,4 +1,7 @@
 import sqlite3
+import http.server
+import socketserver
+from urllib.parse import urlparse, parse_qs
 
 #Connects to database
 Connection = sqlite3.connect('botc_database.db')
@@ -39,7 +42,7 @@ def insertData(character,alignment,win):
     elif len(character) > 20:
         print("Character name must be less than 20 characters")
         return
-    elif ['<', '>', '/', '\\', ':', '*', '?', '"', '|', '!', '@', '#', '$', '%', '^', '&', '(', ')',' '] in character:
+    if any(char in character for char in ['<', '>', '/', '\\', ':', '*', '?', '"', '|', '!', '@', '#', '$', '%', '^', '&', '(', ')', ' ']):
         print("Character name must not contain special characters or spaces")
         return
     elif character.isalpha() == False:
@@ -65,6 +68,35 @@ def insertData(character,alignment,win):
     except sqlite3.Error as error:
         #Prints error message if insert fails
         print("Error while inserting data into SQLite table", error)
+
+#Defines port
+PORT = 8000
+
+class CustomHandler(http.server.SimpleHTTPRequestHandler):
+    '''
+    Custom HTTP request handler that handles GET requests and processes form submissions.
+    '''
+    def do_GET(self):
+        #If the form is submitted, grabs the data
+        if self.path.startswith('/submit'):
+            query = urlparse(self.path).query
+            data = parse_qs(query)
+            character = data.get('character', [''])[0]
+            alignment = data.get('alignment', [''])[0]
+            win = data.get('win', [''])[0]
+            insertData(character, alignment, win)
+            # Respond with a success message
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(f"<html><body><h1>Data submitted</h1><p>make this better</p></body></html>".encode('utf-8'))
+        else:
+            super().do_GET()
+
+#Starts the server
+with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
+    print(f"Serving HTTP on port {PORT}")
+    httpd.serve_forever()
 
 #Closes connection
 cursor.close()
